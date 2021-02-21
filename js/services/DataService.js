1231f7fffdbe603a88536b65b264cda0291914bb
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-02-21 18:14:32 +0100

    Se refactorizan los métodos register y login del Data Service en un único método post

--- js/services/DataService.js
+++ js/services/DataService.js
@@ -4,6 +4,26 @@ const BASE_URL = "http://localhost:8000"
 
 
 export default{
+
+    post: async function (url, postData)  {
+        const config = {
+            method: "POST",
+            headers:{"Content-Type": "application/json"}, //Informo al servidor que los datos que le estoy enviando son en formato JSON
+            body: JSON.stringify(postData)// convierto el objeto de ususarios a un JSON
+        };
+        
+        const response = await fetch(url, config);
+        const data = await response.json(); // recibo respuesta del servidor ya sea OK o sea un error. Por ejemplo que ese usuario ya existe
+        if (response.ok){
+        
+            return data
+        } else {
+           
+            throw new Error(data.message || JSON.stringify(data)); // Si el objeto data contiene un atributo message va a devolverlo, si data.message no existe devolverá undefined y por ello le pedimos que en ese caso devuelva
+        }
+    },
+
+
     getAds: async function (){
         
       //Aquí solo capturamos el error  cuando el servidor sí responde. Si el servidor no responde, capturaremos ese error desde fuera (en index.js cuando llamamos a este método de nuestro DAta Servie)
@@ -19,23 +39,14 @@ export default{
 
 
     registerUser: async function (user){
-        //Utilizamos sparrest para registrar un usuario a través de su end point POST/AUTH/REGISTER
-        
-        const config = {
-            method: "POST",
-            headers:{"Content-Type": "application/json"}, //Informo al servidor que los datos que le estoy enviando son en formato JSON
-            body: JSON.stringify(user)// convierto el objeto de ususarios a un JSON
-        };
         const url = `${BASE_URL}/auth/register`;
-        const response = await fetch(url, config);
-        const data = await response.json(); //recibo respuesta del servidor ya sea OK o sea un error. Por ejemplo que ese usuario ya existe
-        if (response.ok){
-        
-            return data
-        } else {
-           
-            throw new Error(data.message || JSON.stringify(data)); // Si el objeto data contiene un atributo message va a devolverlo, si data.message no existe devolverá undefined y por ello le pedimos que en ese caso devuelva
-        }
+        return await this.post(url, user);
+    },
+
+    login: async function (user){
+      
+        const url = `${BASE_URL}/auth/login`;
+        return await this.post(url,user);
     }
 
 }
